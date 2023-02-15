cd0a7818db1145a9ed70c2833662e87b5db410e1
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-14 20:18:39 -0500

    completed initial Phrase class

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,12 +1,31 @@
-"""
-Create the Phrase class in the phrase.py file
-The class should include an initializer or def __init__ that receives a phrase parameter and holds this phrase in an instance attribute on the Phrase object.
+class Phrase:
 
-phrase: this is the actual phrase the Phrase object is representing. This attribute should be set to the phrase parameter but converted to all lowercase.
+    guessed_letters = []
 
-The class should also have these methods:
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+        self.correct_letters = list(set(self.phrase.replace(" ", "")))
 
-    display(): this prints out the phrase to the console with guessed letters visible and unguessed letters as underscores. For example, if the current phrase is "hello world" and the user has guessed the letter "o", the output should look like this: _ _ _ _ o    _ o _ _ _
-    check_letter(): checks to see if the letter selected by the user matches a letter in the phrase.
-    check_complete(): checks to see if the whole phrase has been guessed.
-"""
+    def display(self):
+        split_phrase = list(self.phrase)
+        phrase_display = list('_' * len(split_phrase))
+        for index, letter in enumerate(split_phrase):
+            if letter == ' ':
+                phrase_display[index] = ' '
+            if letter in self.guessed_letters:
+                phrase_display[index] = letter
+        return f"Current Progress:\n{''.join(phrase_display)}"
+
+    def check_letter(self, letter):
+        return letter in self.correct_letters
+
+    def check_complete(self):
+        return self.correct_letters.sort() == self.guessed_letters.sort()
+
+
+if __name__ == '__main__':
+    # python3 phrasehunter/phrase.py
+    phrase = Phrase('Hello World')
+    print(phrase.display())
+    print(phrase.check_letter('w'))
+    print(phrase.check_complete())
