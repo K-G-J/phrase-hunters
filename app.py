92479fa064599222f7efc7135927d9d74d583fec
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-15 20:51:40 -0500

    added replay prompting

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ if __name__ == '__main__':
     game = Game()
     game.start()
     while (replay := (input("Would you like to play again? (y/n)  ")).lower()) != "n":
-        game = Game()
-        game.start()
+        if replay == "y":
+            game = Game()
+            game.start()
+        else:
+            print('\nPlease enter either "y" or "n"')
     else:
         print("\nThank you for playing Phrase Hunters 👋\n")
